9c6d882ecee296a9a1f0c71576888fb7d62e1c77
supermanc88 <supermanc88@gmail.com>
2020-05-27 10:29:47 +0800

    first commit

---
+++ heartbeat-framework/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by CHM on 5/21/2020.
+//
+
+#include "heartbeat-config.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Version:" << HEARTBEAT_VERSION << endl;
+    cout << "111111111" << endl;
+    return 0;
+}
